83abe0db728178ae5307ab98ab783f8a2049f4ca
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-01-01 13:14:45 -0600

    fix page title/breadcrumbs

--- packages/frontend/app/dashboard/compliance/page.tsx
+++ packages/frontend/app/dashboard/compliance/page.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import type { Metadata } from "next";
+import PageBreadCrumb from "@/components/page-breadcrumb";
 
 export const metadata: Metadata = {
   title: "Releef — Compliance",
 };
 export default function Page() {
-  return <div>Compliance</div>;
+  return (
+    <>
+      <PageBreadCrumb title="Compliance" />
+      <div>Compliance</div>
+    </>
+  );
 }
--- packages/frontend/app/dashboard/reports/page.tsx
+++ packages/frontend/app/dashboard/reports/page.tsx
@@ -9,30 +9,34 @@ import {
 } from "@/components/ui/table";
 import { FileTextIcon } from "lucide-react";
 import { listReports } from "./action";
+import PageBreadCrumb from "@/components/page-breadcrumb";
 
 export default async function Page() {
   const reports = await listReports();
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">File name</TableHead>
-          <TableHead className="text-right">Date uploaded</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {reports.map((report) => (
-          <TableRow key={report.PK}>
-            <TableCell className="font-medium flex items-center">
-              <FileTextIcon className="w-4 h-4 mr-2" />
-              {report.title}
-            </TableCell>
-            <TableCell className="text-right">{report.dateCreated}</TableCell>
+    <>
+      <PageBreadCrumb title="Reports & Insights" />
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">File name</TableHead>
+            <TableHead className="text-right">Date uploaded</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter></TableFooter>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {reports.map((report) => (
+            <TableRow key={report.PK}>
+              <TableCell className="font-medium flex items-center">
+                <FileTextIcon className="w-4 h-4 mr-2" />
+                {report.title}
+              </TableCell>
+              <TableCell className="text-right">{report.dateCreated}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter></TableFooter>
+      </Table>
+    </>
   );
 }
