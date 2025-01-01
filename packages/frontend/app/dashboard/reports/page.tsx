8feaf62e557d395fcef23f6744bf1fa7111d1359
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-01-01 12:54:52 -0600

    create ui for report list

--- packages/frontend/app/dashboard/reports/page.tsx
+++ packages/frontend/app/dashboard/reports/page.tsx
@@ -1,9 +1,38 @@
-import React from "react";
-import type { Metadata } from "next";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { FileTextIcon } from "lucide-react";
+import { listReports } from "./action";
 
-export const metadata: Metadata = {
-  title: "Releef — Reports",
-};
-export default function Page() {
-  return <div>Reports</div>;
+export default async function Page() {
+  const reports = await listReports();
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">File name</TableHead>
+          <TableHead className="text-right">Date uploaded</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {reports.map((report) => (
+          <TableRow key={report.PK}>
+            <TableCell className="font-medium flex items-center">
+              <FileTextIcon className="w-4 h-4 mr-2" />
+              {report.title}
+            </TableCell>
+            <TableCell className="text-right">{report.dateCreated}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter></TableFooter>
+    </Table>
+  );
 }
