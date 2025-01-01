9e40a21c723db045f5bc042c9e302917071c8b3d
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-01-01 12:54:52 -0600

    create ui for report list

---
+++ packages/frontend/app/dashboard/reports/action.ts
@@ -0,0 +1,26 @@
+"use server";
+import { auth } from "@/auth";
+import { Session, Report } from "@releef.ai/types";
+const CORE_API_URL = process.env.CORE_API_URL;
+
+export const listReports = async () => {
+  const session = (await auth()) as Session;
+
+  const response = await fetch(
+    `${CORE_API_URL}/report?userId=${session.user.id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to get reports");
+  }
+
+  const data = await response.json();
+
+  return data.items as Report[];
+};
