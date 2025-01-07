3b8a03fb174385dee20b0d7660dcedb4652bb938
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-01-06 18:35:28 -0600

    create ui for report compliance page

---
+++ packages/frontend/app/dashboard/report/[id]/page.tsx
@@ -0,0 +1,14 @@
+import PageBreadCrumb from "@/components/page-breadcrumb";
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  return (
+    <>
+      <PageBreadCrumb title="Report" />
+      <div>Report ID: {id}</div>
+    </>
+  );
+}
