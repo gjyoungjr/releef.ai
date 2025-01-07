e9afb22b62ae6ca0e46b3fecf3410380c07d911e
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-01-06 20:31:54 -0600

    add get report api call to ui

--- packages/frontend/app/dashboard/report/[id]/page.tsx
+++ packages/frontend/app/dashboard/report/[id]/page.tsx
@@ -1,10 +1,14 @@
 import PageBreadCrumb from "@/components/page-breadcrumb";
+import { getReport } from "./action";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const id = (await params).id;
+  const report = await getReport(id);
+
   return (
     <>
       <PageBreadCrumb title="Report" />
