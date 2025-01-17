b63671883f4d5e6604ff5f7abe9ce3978e410f88
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-01-17 14:43:11 -0600

    started work on embedding reports

--- packages/backend/lib/stacks/file-ingestor-stack.ts
+++ packages/backend/lib/stacks/file-ingestor-stack.ts
@@ -10,6 +10,8 @@ import { CoreStackProps } from "../../environment";
 import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 import { Table } from "aws-cdk-lib/aws-dynamodb";
 import { NodeJSLambda } from "../constructs";
+import { PythonFunction } from "@aws-cdk/aws-lambda-python-alpha";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 dotenv.config();
 
@@ -42,17 +44,17 @@ export class FileIngestorStack extends cdk.Stack {
       retentionPeriod: cdk.Duration.days(4), // Messages are retained for 4 days if not processed
     });
 
-    // const embedDocQueue = new sqs.Queue(this, "EmbedDocQueue", {
-    //   visibilityTimeout: cdk.Duration.minutes(TIMEOUT_IN_MINUTES), // Match the Lambda timeout to avoid message being reprocessed
-    //   retentionPeriod: cdk.Duration.days(4), // Messages are retained for 4 days if not processed
-    // });
+    const embedDocQueue = new sqs.Queue(this, "EmbedDocQueue", {
+      visibilityTimeout: cdk.Duration.minutes(TIMEOUT_IN_MINUTES), // Match the Lambda timeout to avoid message being reprocessed
+      retentionPeriod: cdk.Duration.days(4), // Messages are retained for 4 days if not processed
+    });
 
     fileIngestTopic.addSubscription(
       new sns_subscriptions.SqsSubscription(storeReportQueue)
     );
-    // fileIngestTopic.addSubscription(
-    //   new sns_subscriptions.SqsSubscription(embedDocQueue)
-    // );
+    fileIngestTopic.addSubscription(
+      new sns_subscriptions.SqsSubscription(embedDocQueue)
+    );
 
     this.filesBucket.addEventNotification(
       s3.EventType.OBJECT_CREATED_PUT,
@@ -82,28 +84,28 @@ export class FileIngestorStack extends cdk.Stack {
     //   },
     // });
 
-    // const embedDocument = new PythonFunction(this, "EmbedDocument", {
-    //   entry: `lib/lambda/document/embed-doc`,
-    //   index: "main.py",
-    //   runtime: Runtime.PYTHON_3_9,
-    //   description: "Embed document.",
-    //   timeout: cdk.Duration.minutes(TIMEOUT_IN_MINUTES),
-    //   bundling: {
-    //     // translates to `rsync --exclude='.venv'`
-    //     assetExcludes: [".venv"],
-    //   },
-    //   // environment: {
-    //   //   PINECONE_API_KEY: process.env.PINECONE_API_KEY!,
-    //   //   PINECONE_INDEX_NAME: process.env.PINECONE_INDEX_NAME!,
-    //   //   CORE_TABLE_NAME: props.coreTable.tableName,
-    //   // },
-    // });
+    const embedDocument = new PythonFunction(this, "EmbedDocument", {
+      entry: `lib/lambda/document/embed-doc`,
+      index: "main.py",
+      runtime: Runtime.PYTHON_3_9,
+      description: "Embed document.",
+      timeout: cdk.Duration.minutes(TIMEOUT_IN_MINUTES),
+      bundling: {
+        // translates to `rsync --exclude='.venv'`
+        assetExcludes: [".venv"],
+      },
+      environment: {
+        PINECONE_API_KEY: process.env.PINECONE_API_KEY!,
+        PINECONE_INDEX_NAME: process.env.PINECONE_INDEX_NAME!,
+        CORE_TABLE_NAME: props.coreTable.tableName,
+      },
+    });
 
     props.coreTable.grantReadWriteData(storeReport);
     this.filesBucket.grantReadWrite(storeReport);
-    // this.filesBucket.grantReadWrite(embedDocument);
+    this.filesBucket.grantReadWrite(embedDocument);
 
     storeReport.addEventSource(new SqsEventSource(storeReportQueue));
-    // embedDocument.addEventSource(new SqsEventSource(embedDocQueue));
+    embedDocument.addEventSource(new SqsEventSource(embedDocQueue));
   }
 }
