8d46630165aaaec68a9b10151fffddd8e58a4b44
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-01-21 16:52:51 -0600

    ui fixes on landing page

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,11 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
+const FEATURES = [
+  {
+    title: "Gap Analysis",
+    description:
+      "Uncover compliance gaps in your sustainability reports and ensure alignment with industry standards and regulations.",
+  },
+  {
+    title: "Recommendations",
+    description:
+      "Receive tailored recommendations to address compliance gaps and enhance sustainability performance.",
+  },
+  {
+    title: "KPI Tracking",
+    description:
+      "Monitor and analyze key sustainability metrics to track your progress toward compliance and impact goals.",
+  },
+];
+
 export default function Page() {
   return (
-    <div className="overflow-y-hidden">
-      <div className={"grain-blur background-base"} />
+    <>
+      <div className={"grain-blur background-base dark"} />
       <div className={"grain-background background-base"} />
       <div className={"grid-bg background-base"} />
       <div className={"large-blur background-base"} />
@@ -13,7 +32,7 @@ export default function Page() {
       <header className="fixed left-0 right-0 top-0 z-10 flex h-14 items-center p-4">
         <nav className="flex items-center space-x-4 lg:space-x-6">
           <Link href="/" className="group flex w-full items-center gap-x-2.5">
-            <div className="flex aspect-square size-7 items-center justify-center rounded-lg bg-primary text-sidebar-primary-foreground">
+            <div className="flex aspect-square size-7 items-center justify-center rounded-lg border text-sidebar-primary-foreground">
               <span className="text-lg">🍃</span>
             </div>
             <h3 className="font-semibold tracking-wide">Releef</h3>
@@ -27,12 +46,12 @@ export default function Page() {
         </div>
       </header>
 
-      <section className="mx-auto max-w-7xl px-[32px] relative flex items-center justify-center h-screen overflow-hidden">
+      <section className="mx-auto max-w-7xl px-[32px] relative flex pt-52 justify-center">
         <div className={"text-center w-full "}>
-          <h1 className="text-[48px] leading-[48px] md:text-[80px] md:leading-[80px] tracking-[-1.6px] font-bold  ">
-            AI Gap Analysis
+          <h1 className="text-[45px] leading-[48px] md:text-[65px] md:leading-[65px] tracking-[-1.8px] font-bold  ">
+            Achieve sustainability compliance
             <br />
-            for sustainability reports.
+            in minutes, not months.
           </h1>
           <p className="mt-6 text-[18px] leading-[27px] md:text-[20px] md:leading-[30px]">
             Identify compliance gaps, streamline reporting, and drive
@@ -41,14 +60,24 @@ export default function Page() {
         </div>
       </section>
 
-      <footer className="fixed left-0 right-0 bottom-0 z-10 flex h-14 items-center p-4">
-        <Link href="https://x.com/gjyoungjr" target="_blank">
-          Twitter
-        </Link>
-        <div className="ml-auto">
-          <p className="">&copy; Releef Inc.</p>
-        </div>
-      </footer>
-    </div>
+      <div className="isolate mx-auto grid grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3 p-10">
+        {FEATURES.map((feature, index) => (
+          <div
+            className={cn(
+              "rounded-lg bg-inherit backdrop-blur-[6px] overflow-hidden"
+            )}
+          >
+            <div
+              className={cn(
+                "flex gap-5 flex-col rounded-lg rounded-b-none pricing-card-border p-5"
+              )}
+            >
+              <p className="font-semibold">{feature.title}</p>
+              <p>{feature.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
