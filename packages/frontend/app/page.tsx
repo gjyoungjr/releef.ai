bf9261ea118584ded6d89b7d9d533cddaf7e0589
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-02-17 06:33:54 -0600

    update landing page

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -24,8 +24,8 @@ export default function Home() {
             </h1>
 
             <p className="text-white/80 text-xl mb-12 max-w-2xl mx-auto">
-              Simplify ESG reporting and compliance with AI-powered automation
-              designed for today&apos;s businesses.
+              Take the complexity out of ESG compliance with AI-powered
+              automation, saving you time and ensuring regulatory success.
             </p>
 
             <Link href="https://cal.com/gyoung/30min" target="_blank">
